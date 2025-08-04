5bbccbf4e8687e33afbfaad82bb5ef5c36cc5e7d
Your Name <your@email.com>
2025-08-04 22:56:03 +0530

    initial commit

---
+++ subarray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class subarray {
+    public static void printsubarray(int number[]) {
+         
+        int totalsubarray = 0;
+        for(int i=0; i<number.length; i++) {
+            int start = i;
+            for(int j=i; j<number.length; j++) {
+                int end = j;
+                for(int  k=start; k<=end; k++) {
+                    System.out.print(number[k]+" ");
+                }
+                totalsubarray++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("total subarray = " + totalsubarray);
+    }
+    
+    public static void main(String args[]) {
+        int number[] = {2, 4, 6, 8, 10};
+        printsubarray(number);
+
+    }
+}
